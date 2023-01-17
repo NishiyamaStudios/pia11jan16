0d9690a8d65b6e8f45926fb697a72c4cce2312af
NishiyamaStudios <jcwasterberg@gmail.com>
2023-01-17 21:00:02 +0100

    updates

--- app/src/main/java/se/nishiyamastudios/myapplication/MainActivity.kt
+++ app/src/main/java/se/nishiyamastudios/myapplication/MainActivity.kt
@@ -2,14 +2,35 @@ package se.nishiyamastudios.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
+    var personadapter = PersonAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        personadapter.people.add("Xerxes")
+        personadapter.people.add("Banan")
+
+        val personRecview = findViewById<RecyclerView>(R.id.personsRV)
+
+        personRecview.adapter = personadapter
+        personRecview.layoutManager = LinearLayoutManager(this)
+
+
+        findViewById<Button>(R.id.addPersonButton).setOnClickListener() {
+
+            var addname = findViewById<EditText>(R.id.addPersonET).text.toString()
+
+            personadapter.people.add(addname)
+
+        }
 
     }
 }
--- app/src/main/java/se/nishiyamastudios/myapplication/PersonAdapter.kt
+++ app/src/main/java/se/nishiyamastudios/myapplication/PersonAdapter.kt
@@ -1,18 +1,31 @@
 package se.nishiyamastudios.myapplication
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class PersonAdapter : RecyclerView.Adapter<PersonAdapter.ViewHolder>() {
 
+    var people = mutableListOf<String>("Arne","Bartil","Caesar")
+
     class ViewHolder(view : View) : RecyclerView.ViewHolder(view) {
+        val personName: TextView
+
+        init {
+
+            personName = view.findViewById(R.id.personNameTV)
+
+        }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         // viewType: Int = kan definiera olika typer av rader, produktrad, headerrad etc..
+
+        Log.i("pia11debug","SKAPA RAD")
 
         val view = LayoutInflater.from(parent.context).inflate(R.layout.person_item, parent, false)
 
@@ -20,10 +33,13 @@ class PersonAdapter : RecyclerView.Adapter<PersonAdapter.ViewHolder>() {
     }
 
     override fun getItemCount(): Int {
-        return 5
+        return people.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        Log.i("pia11debug","RITA RAD " + position.toString())
+
+        holder.personName.text = people[position]
 
     }
 
